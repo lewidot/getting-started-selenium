a6af96114f985302884a7ad93095bc86049becb8
Lewis Evans <lewisevansuk@gmail.com>
2024-07-19 17:31:05 +0100

    update selenium version and add main func

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com")
-print(driver.title)
-driver.close()
+
+def main() -> None:
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.google.com")
+    print(driver.title)
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
